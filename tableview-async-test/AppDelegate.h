cd7a31c1ae9fd2d38d104f934c57e47e514816d1
Sergey Buravtsov <serobur@gmail.com>
2015-07-17 11:10:56 +0300

    Initial commit

---
+++ tableview-async-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableview-async-test
+//
+//  Created by Sergey Buravtsov on 17.07.15.
+//  Copyright (c) 2015 Sergey Buravtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
