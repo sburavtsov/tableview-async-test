4f0bdcaa43134f192f916dfbe6dc46cbca376ab9
Sergey Buravtsov <serobur@gmail.com>
2015-07-17 12:50:14 +0300

    Intial PersonModel added

---
+++ tableview-async-test/models/PersonModel.h
@@ -0,0 +1,13 @@
+//
+//  PersonModel.h
+//  tableview-async-test
+//
+//  Created by Sergey Buravtsov on 17.07.15.
+//  Copyright (c) 2015 Sergey Buravtsov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonModel : NSObject
+
+@end
---
+++ tableview-async-test/models/PersonModel.m
@@ -0,0 +1,13 @@
+//
+//  PersonModel.m
+//  tableview-async-test
+//
+//  Created by Sergey Buravtsov on 17.07.15.
+//  Copyright (c) 2015 Sergey Buravtsov. All rights reserved.
+//
+
+#import "PersonModel.h"
+
+@implementation PersonModel
+
+@end
