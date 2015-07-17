a0e1f6e7782bc2e0fea392c15a06c0a44ce41959
Sergey Buravtsov <serobur@gmail.com>
2015-07-17 12:51:07 +0300

    UI connections done

--- tableview-async-test/ViewController.h
+++ tableview-async-test/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISwitch *asyncUpdatesSwitch;
 
 @end
 
